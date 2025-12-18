fb8e3d5025ac9c40cf38a6632b046d3b8e8cbf3f
Mohammed Kareemullah <mohammedkareemullah2005@gmail.com>
2025-12-18 22:59:07 +0530

    Created Transactions ViewModel for CRUD operations

--- lib/features/expense_tracking/view/pages/transactions_page.dart
+++ lib/features/expense_tracking/view/pages/transactions_page.dart
@@ -18,7 +18,56 @@ class _TransactionsPageState extends ConsumerState<TransactionsPage> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 12.0),
-        child: Column(children: [const SizedBox(height: 20)]),
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            Expanded(
+              child: transactions.when(
+                data: (transactions) {
+                  if (transactions.isEmpty) {
+                    return const Center(
+                      child: Text(
+                        "No Transactions yet!",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    );
+                  }
+
+                  return ListView.builder(
+                    itemCount: transactions.length,
+                    scrollDirection: Axis.vertical,
+                    itemBuilder: (context, index) {
+                      final trans = transactions[index];
+                      return TransactionCard(
+                        title: trans.title,
+                        value: trans.value,
+                        date: trans.date,
+                        type: trans.type,
+                        category: trans.category,
+                      );
+                    },
+                  );
+                },
+                error: (error, stackTrace) {
+                  return Center(
+                    child: Text(
+                      "Error : $error",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  );
+                },
+                loading: () =>
+                    const Center(child: CircularProgressIndicator.adaptive()),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
